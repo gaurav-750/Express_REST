60b54c4f1c1b8a9cbf4ed909faedbc8fbb0dc930
Gaurav <gauravsomani52750@gmail.com>
2024-02-18 21:35:46 +0530

    created a user model

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      required: true,
+    },
+
+    //* reference to Post
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  {
+    timestamps: true, //? mongoose will automatically add 'createdAt' and 'updatedAt' fields to the schema
+  }
+);
+
+//create Post model
+const Post = mongoose.model("Post", PostSchema);
+module.exports = Post;
